7e16109cc7b43d13fbb223f84664efb97a31ed03
Hans Rivera <hrivera@unal.edu.co>
2023-07-23 21:07:45 -0500

    feat: added docker compose file to handle postgres sql

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { EnvConfiguration } from './config/env.config';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  Logger.log(EnvConfiguration);
   await app.listen(3001);
 }
 bootstrap();
